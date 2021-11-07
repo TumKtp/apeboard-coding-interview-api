b33f3ea720d98f10394ba125652a53e3457cff69
TumKtp <tumktp.dev@gmail.com>
2021-11-07 22:11:04 +0700

    Optimize time complexity

--- config/skipPools.js
+++ config/skipPools.js
@@ -1,1 +1,5 @@
-module.exports = [444, 105];
+module.exports = [64, 105, 444];
+// Error
+// pid 64: WBNB - Pancake Factory
+// pid 105: Not Pancake LPs contract
+// pid 444: LP Address: 0x0000000000000000000000000000000000000064
--- state/updateTokenSymbols.js
+++ state/updateTokenSymbols.js
@@ -1,18 +1,33 @@
 const { getTokenSymbol } = require("../utils/erc20");
 const { promises: fs } = require("fs");
+const ERC20Abi = require("../config/abi/erc20.json");
+const multicall = require("../utils/multicall");
 
 exports.updateTokenSymbols = async (tokenAddresses) => {
+  // Check latest token symbols.
   let rawData;
   try {
     rawData = await fs.readFile("tokenSymbols.json");
   } catch (e) {}
   const tokenSymbols = rawData ? JSON.parse(rawData) : {};
-  let updated = false;
+
+  // Call: All token symbols
+  const symbolsCalls = [];
+  tokenAddresses = [...new Set(tokenAddresses)];
   for (let i = 0; i < tokenAddresses.length; i++) {
     if (tokenAddresses[i] in tokenSymbols) continue;
+    symbolsCalls.push({
+      address: tokenAddresses[i],
+      name: "symbol",
+    });
+  }
+  const symbols = await multicall(ERC20Abi, symbolsCalls);
+
+  let updated = false;
+  for (let i = 0; i < symbolsCalls.length; i++) {
+    if (symbolsCalls[i].address in tokenSymbols) continue;
+    tokenSymbols[symbolsCalls[i].address] = symbols[i][0];
     updated = true;
-    const symbol = await getTokenSymbol(tokenAddresses[i]);
-    tokenSymbols[tokenAddresses[i]] = symbol;
   }
   if (updated)
     return await fs.writeFile(
